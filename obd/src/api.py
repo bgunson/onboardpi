2408b61752d7d756611e3eb3561c0d1eebeb18dd
bennett.gunson <bennett.gunson@cs.ucalgary.ca>
2023-01-25 22:34:53 -0700

    fix spelling in supports api event handler

--- obd/src/api.py
+++ obd/src/api.py
@@ -126,7 +126,7 @@ class API:
             if obd.commands.has_name(cmd):
                 await sio.emit('supports', self.config.obd_io.supports(obd.commands[cmd]), room=sid)
             else:
-                await sio.emit('suuports', False, room=sid)
+                await sio.emit('suports', False, room=sid)
 
         @sio.event
         async def protocol_id(sid):
