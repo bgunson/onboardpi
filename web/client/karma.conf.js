0aaca5d2ffad0ba5bb53ae5126ba2269274ef472
Bennett Gunson <bengunson@gmail.com>
2023-02-05 16:36:56 -0700

    re-add chromeheadless to karma config

--- web/client/karma.conf.js
+++ web/client/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: [
-      // 'ChromeHeadless' 
+      'ChromeHeadless' 
     ],
     singleRun: false,
     restartOnFileChange: true
