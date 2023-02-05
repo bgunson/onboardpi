c10963a5c329d89e564bc4f7a2248a9d20df2d3e
Bennett Gunson <bengunson@gmail.com>
2023-02-05 16:23:43 -0700

    testing documentation

--- web/client/karma.conf.js
+++ web/client/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),    // does not work on wsl2
+      require('karma-chrome-launcher'),  
      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -38,7 +38,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: [
-      'ChromeHeadless' 
+      // 'ChromeHeadless' 
     ],
     singleRun: false,
     restartOnFileChange: true
